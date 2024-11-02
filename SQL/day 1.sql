9ccfb87c6b4e080549c169b7ad9925e0cc8973ad
Thasleema Banu S <111576542+sthasleemabanu@users.noreply.github.com>
2024-11-02 08:30:31 +0530

    Create day 1.sql

---
+++ SQL/day 1.sql
@@ -0,0 +1,12 @@
+# Employees Inside Office (Part 1)
+
+Questions: A company record its employee's movement In and Out of office in a table. Please note below points about the data:
+          1- First entry for each employee is “in”
+          2- Every “in” is succeeded by an “out”
+          3- Employee can work across days
+
+Write a SQL to find the number of employees inside the Office at “2019-04-01 19:05:00"
+
+# Difficulty Level: MEDIUM
+
+-----------------------Solution-----------------------------------------------------
